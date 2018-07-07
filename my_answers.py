d91b5884fd6009cbbc26d633c23a2f9261a88333
Ramakrishna <mramakrishna.mrk@gmail.com>
2018-07-07 22:52:15 +0530

    updated

--- my_answers.py
+++ my_answers.py
@@ -141,7 +141,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1500
-learning_rate = 0.1
+iterations = 5000
+learning_rate = 0.5
 hidden_nodes = 20
 output_nodes = 1
